bb66269334e0c31d0789799ad3bb915e4022cbc3
Turqay Abdullayev <turgaj.abdullajev@gmail.com>
2026-03-22 03:27:58 +0400

    docs: add docs to query builder

--- mongoeb/core/services/query_builder.py
+++ mongoeb/core/services/query_builder.py
@@ -1,6 +1,24 @@
 from datetime import datetime
 
-def parse_value(value: str):
+def parse_value(value: str) -> int | float | bool | datetime | str:
+    """
+    Convert string CLI input into appropriate Python type.
+
+    Attempts to get the most suitable type for a given string value.
+
+    Order:
+    1. Integer ("69" -> 69)
+    2. Float ("3.14" -> 3.14)
+    3. Bool ("true" -> True, "false" -> False)
+    4. Datetime (ISO string -> datetime obj)
+    5. Fallback (remains string)
+
+    Important:
+    Datetime parsing uses `datetime.fromisoformat`, so input most follow ISO format
+
+    :param value: Raw string value from CLI.
+    :return: Parse value in appropriate Python type.
+    """
     if value.isdigit():
         return int(value)
     try:
@@ -17,7 +35,25 @@ def parse_value(value: str):
 
     return value
 
-def build_query(filters: list[str]) -> dict:
+def build_query(filters: list[str]) -> dict[str, str]:
+    """
+    Construct a MongoDB query dict using CLI key-value pairs
+
+    Takes list of CLI arguments representing altering keys and values and transforms into dict.
+
+    Example:
+        input:
+            filters = ["benefit", "Free Will", "description", "Very descriptive"]
+
+        Output:
+            {
+                "benefit": "Free Will",
+                "description": "Very descriptive"
+            }
+
+    :param filters: list of CLI
+    :return: dict representing MongoDB query
+    """
     query = {}
 
     for i in range(0, len(filters), 2):
