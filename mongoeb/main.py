ebfb9cad707f79c2bec0b1c31053a90d351aaef1
Turqay Abdullayev <turgaj.abdullajev@gmail.com>
2026-03-22 22:42:45 +0400

    chore: update pyproject and main

--- mongoeb/main.py
+++ mongoeb/main.py
@@ -1,11 +1,22 @@
-from dotenv import load_dotenv
-from mongoeb.cli.commands import app
+import os
 
-load_dotenv()
+import typer
+from dotenv import load_dotenv
+from mongoeb.cli.commands import app as commands_app
+
+
+
+app = typer.Typer(help="MongoDB exploration CLI")
+app.add_typer(commands_app)
 
 
 def main():
     load_dotenv()
+
+    if not os.getenv("MONGO_URI"):
+        typer.echo("Error: MONGO_URI not set")
+        raise typer.Exit(code=1)
+
     app()
 
 
