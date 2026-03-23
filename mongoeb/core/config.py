b45f280fce606abd6bbfa10e51c4ed46db5d8d19
Turqay Abdullayev <turgaj.abdullajev@gmail.com>
2026-03-23 07:15:33 +0400

    feat: add file config support in load_config

--- mongoeb/core/config.py
+++ mongoeb/core/config.py
@@ -1,6 +1,8 @@
 import os
 from dataclasses import dataclass
 
+from mongoeb.config_file import load_config_file
+
 
 @dataclass(frozen=True)
 class Config:
@@ -20,13 +22,19 @@ class Config:
 
 
 def load_config() -> Config:
+
+    file_config = load_config_file()
+
     config = Config(
-        database=os.getenv("M_DATABASE"),
-        username=os.getenv("M_USERNAME"),
-        password=os.getenv("M_PASSWORD"),
-        host=os.getenv("M_HOST"),
-        port=int(os.getenv("M_PORT")),
-        scheme=os.getenv("M_SCHEME"),
+        database=get_value("M_DATABASE", "database", file_config),
+        username=get_value("M_USERNAME", "username", file_config),
+        password=get_value("M_PASSWORD", "password", file_config),
+        host=get_value("M_HOST", "host", file_config),
+        port=int(get_value("M_PORT", "port", file_config)),
+        scheme=get_value("M_SCHEME", "scheme", file_config),
     )
     config.validate()
     return config
+
+def get_value(env_key: str, file_key: str, file_config: dict):
+    return os.getenv(env_key) or file_config.get(file_key)
